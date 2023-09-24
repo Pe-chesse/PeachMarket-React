ff8b778da5967d84f6025f87fd3226d4394d621f
JayG-5 <jg55552758@gmail.com>
2023-09-24 16:59:46 +0900

    fire(chat): remove deadcode

--- src/pages/chat/room.js
+++ src/pages/chat/room.js
@@ -1,8 +1,21 @@
 import { Link } from "react-router-dom";
+import { useEffect, useState} from 'react';
 import Navbar from "../../components/navbar";
 import ChatRoomTile from "../../components/chat/chatroom-tile";
 
-function ChatList({chatState}) {
+function ChatRoom({chatState}) {
+    // const [posts, setPosts] = useState([]);
+    // useEffect( () => {
+    //     async function fetchData() {
+    //         try {
+    //             const response = await api.post.list();
+    //             setPosts(response);
+    //         } catch (error) {
+    //             console.log(error);
+    //         }
+    //     }
+    //     fetchData();
+    //   }, []);
     return (
         <div className="home-wrapper">
         <article className="top-area">
