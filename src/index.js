b67daaa839620d6da9e28a2a8916069bcc7d3418
Seungil Jung <jungsi1217@naver.com>
2024-04-10 23:48:01 +0900

    chore(Router BaseName)

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import GlobalStyle from './styles/global';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Router>
+  <Router basename={process.env.PUBLIC_URL}>
     <GlobalStyle/>
     <App/>
   </Router>
